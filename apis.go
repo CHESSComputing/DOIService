5a0b7e67ec4c1e71bccad6f11367e7b6b3cdc5f4
Valentin Kuznetsov <vkuznet@gmail.com>
2025-06-05 13:49:49 -0400

    add helper function selectKeys

--- apis.go
+++ apis.go
@@ -2,20 +2,38 @@ package main
 
 import (
 	srvConfig "github.com/CHESSComputing/golib/config"
+	"github.com/CHESSComputing/golib/utils"
 )
 
 // getRecords fetches records from the database based on the given ID
 func getRecords(doiPattern string) []map[string]any {
 	spec := make(map[string]any)
 	pat := make(map[string]any)
-	pat["$regex"] = doiPattern
-	pat["$options"] = "i" // Case-insensitive search
-	spec["doi"] = pat
+	if doiPattern != "" {
+		pat["$regex"] = doiPattern
+		pat["$options"] = "i" // Case-insensitive search
+		spec["doi"] = pat
+	}
 	dbname := srvConfig.Config.CHESSMetaData.MongoDB.DBName
 	collname := srvConfig.Config.CHESSMetaData.MongoDB.DBColl
 	idx := 0
 	limit := -1
 	return metaDB.Get(dbname, collname, spec, idx, limit)
+}
+
+// helper function to select keys from a record and return final list of reduced records
+func selectKeys(records []map[string]any, keys []string) []map[string]any {
+	var out []map[string]any
+	for _, rec := range records {
+		newRec := make(map[string]any)
+		for key, val := range rec {
+			if utils.InList(key, keys) {
+				newRec[key] = val
+			}
+		}
+		out = append(out, newRec)
+	}
+	return out
 }
 
 // countDOIRecords returns number of DOI records
